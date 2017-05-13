3936be295860cd7d72cae4893c44434daa4ff485
jnix <jnix@garretsongroup.com>
2017-05-12 21:12:20 -0400

    Update setup file

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup
 
-setup(name='datasciencetools',
-      version='0.1',
+setup(name='sspyrs',
+      version='0.1a2',
       description='Lightweight interface for SSRS reports to python',
+      long_description='',
       url='',
       author='James Nix',
-      author_email='jnix@garretsongroup.com',
+      author_email='james.k.nix@gmail.com',
       license='MIT',
       packages=['sspyrs'],
-      dependency_links=['https://github.com/requests/requests-ntlm'],
       install_requires=[
            'pandas>=0.18.1',
-           'xmltodict==0.10.2'
+           'openpyxl>=2.4.7',
+           'xmltodict>=0.10.2',
+           'requests_ntlm>=1.0.0'
       ],
+      keywords=['ssrs report reporting sql'],
       zip_safe=False)
